ad108e713bedfdf4f166a8420395c2b102aa20a1
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-01 16:34:37 -0300

    [WIP] provision direnv in setup (flext-13z9y)

--- tests/unit/codegen/test_codegen_catalog_extensions.py
+++ tests/unit/codegen/test_codegen_catalog_extensions.py
@@ -100,6 +100,7 @@ class TestsCodegenCatalogExtensions:
         tm.that("_builtin_setup_conform" in content, eq=False)
         setup_env = content.split("_builtin_setup_environment:", 1)[1]
         tm.that("codegen conform" in setup_env.split("\n\n", 1)[0], eq=False)
+        tm.that(content, has='direnv allow "$(PROJECT_ROOT)"')
         tm.that("_builtin_gen_check:" in content, eq=True)
         tm.that("_builtin_gen_apply:" in content, eq=True)
         verb_names = {verb.name for verb in config.Infra.codegen.make.verbs}
