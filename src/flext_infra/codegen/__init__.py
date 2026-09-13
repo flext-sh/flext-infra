56c6b88c6f804422c7976fdb69e957719b825536
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-12 23:21:53 -0300

    codegen: export FlextInfraCodegenLayout for lazy-init pipeline

--- src/flext_infra/codegen/__init__.py
+++ src/flext_infra/codegen/__init__.py
@@ -37,6 +37,7 @@ __all__: tuple[str, ...] = (
     "FlextInfraCodegenConsolidator",
     "FlextInfraCodegenFixer",
     "FlextInfraCodegenGeneration",
+    "FlextInfraCodegenLayout",
     "FlextInfraCodegenLazyInit",
     "FlextInfraCodegenLazyInitGenerationMixin",
     "FlextInfraCodegenLazyInitPlanner",
