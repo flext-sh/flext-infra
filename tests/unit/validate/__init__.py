5402151fe525b9d1fcfd2c2dd8612743c6d98e1e
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-17 02:23:41 -0300

    chore(gen): refresh validate test lazy exports

--- tests/unit/validate/__init__.py
+++ tests/unit/validate/__init__.py
@@ -29,9 +29,20 @@ if TYPE_CHECKING:
     from .silent_failure_tests import TestsFlextInfraSilentFailure
     from .skill_validator_tests import TestsFlextInfraSkillValidator
     from .stub_chain_tests import TestsFlextInfraStubChain
+    from .test_core_validation_behavior import TestsCoreValidationBehavior
+    from .test_fixture_violations import TestsFixtureViolations
     from .test_import_dag_tests import TestsFlextInfraImportDag
+    from .test_module_path_rules import TestsModulePathRules
+    from .test_pydantic_legacy_detection import TestsPydanticLegacyDetection
+    from .test_rule0_namespace_structure import TestsRule0NamespaceStructure
+    from .test_rule1_constants import TestsRule1ConstantsFacade
+    from .test_rule2_typings import TestsRule2TypingsFacade
+    from .test_rule3_imports import TestsRule3ImportRules
+    from .test_rule4_annotations import TestsRule4Annotations
     from .tier_whitelist_tests import TestsFlextInfraTierWhitelist
 __all__: tuple[str, ...] = (
+    "TestsCoreValidationBehavior",
+    "TestsFixtureViolations",
     "TestsFlextInfraFreshImport",
     "TestsFlextInfraGovernanceAuthority",
     "TestsFlextInfraImportCycles",
@@ -52,6 +63,13 @@ __all__: tuple[str, ...] = (
     "TestsFlextInfraValidateCli",
     "TestsFlextInfraValidateInit",
     "TestsFlextInfraValidateMain",
+    "TestsModulePathRules",
+    "TestsPydanticLegacyDetection",
+    "TestsRule0NamespaceStructure",
+    "TestsRule1ConstantsFacade",
+    "TestsRule2TypingsFacade",
+    "TestsRule3ImportRules",
+    "TestsRule4Annotations",
     "c",
     "d",
     "e",
@@ -91,7 +109,16 @@ _LAZY_IMPORTS = MappingProxyType(
             ".silent_failure_tests": ("TestsFlextInfraSilentFailure",),
             ".skill_validator_tests": ("TestsFlextInfraSkillValidator",),
             ".stub_chain_tests": ("TestsFlextInfraStubChain",),
+            ".test_core_validation_behavior": ("TestsCoreValidationBehavior",),
+            ".test_fixture_violations": ("TestsFixtureViolations",),
             ".test_import_dag_tests": ("TestsFlextInfraImportDag",),
+            ".test_module_path_rules": ("TestsModulePathRules",),
+            ".test_pydantic_legacy_detection": ("TestsPydanticLegacyDetection",),
+            ".test_rule0_namespace_structure": ("TestsRule0NamespaceStructure",),
+            ".test_rule1_constants": ("TestsRule1ConstantsFacade",),
+            ".test_rule2_typings": ("TestsRule2TypingsFacade",),
+            ".test_rule3_imports": ("TestsRule3ImportRules",),
+            ".test_rule4_annotations": ("TestsRule4Annotations",),
             ".tier_whitelist_tests": ("TestsFlextInfraTierWhitelist",),
             "flext_tests": (
                 "c",
