8673e7bceeee22843c44cc504c6103bd227f9d68
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-19 21:59:57 -0300

    fix(models): restore relative imports in models.py (fix circular import cascade from wip commit)

--- src/flext_infra/models.py
+++ src/flext_infra/models.py
@@ -18,7 +18,7 @@ from flext_core import m
 if TYPE_CHECKING:
     from flext_cli.models import FlextCliModels
 
-from flext_infra._models._config import FlextInfraConfigModels
+from ._models._config import FlextInfraConfigModels
 
 from ._models.base import FlextInfraModelsBase
 from ._models.census import FlextInfraModelsCensus
