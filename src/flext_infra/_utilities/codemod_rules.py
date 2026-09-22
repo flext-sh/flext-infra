6ee3165092e3c02a9963ebdcd5a3ac1b7e268a11
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 23:17:08 -0300

    [WIP] preserve concurrent codemod import ordering

--- src/flext_infra/_utilities/codemod_rules.py
+++ src/flext_infra/_utilities/codemod_rules.py
@@ -8,10 +8,9 @@ from importlib.metadata import Distribution, distributions
 from importlib.util import find_spec
 from pathlib import Path
 
+from flext_cli import u
 from packaging.requirements import Requirement
 from packaging.utils import canonicalize_name
-
-from flext_cli import u
 
 from .. import c, m, p, r, t
 from .dependencies import FlextInfraUtilitiesDependencies
