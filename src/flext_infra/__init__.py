6148a95e67f18f0774f6cde89ed1fb05d8c369ee
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-23 20:18:18 -0300

    fixes

--- src/flext_infra/__init__.py
+++ src/flext_infra/__init__.py
@@ -20,7 +20,9 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_cli import cli, core, d, e, h, lazy_attribute, r, x
+    from flext_cli import cli
+
+    from flext_core import core, d, e, h, lazy_attribute, r, x
 
     from . import (
         check,
@@ -790,7 +792,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".workspace.orchestrator": ("FlextInfraOrchestratorService",),
             ".workspace.rope": ("FlextInfraRopeWorkspace",),
             ".worktree": ("FlextInfraWorktreeService",),
-            "flext_cli": ("cli", "core", "d", "e", "h", "lazy_attribute", "r", "x"),
+            "flext_cli": ("cli",),
+            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
